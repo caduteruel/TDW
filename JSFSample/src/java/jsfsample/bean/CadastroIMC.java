f186408ab1ac1d3f597f02644a7fbb7bc05da096
CARLOS EDUARDO TERUEL RIBEIRO <162150425@acad.dvuadmin.net>
2018-04-19 20:17:03 -0300

    Commit

---
+++ JSFSample/src/java/jsfsample/bean/CadastroIMC.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsfsample.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author DB1007634
+ */
+@ManagedBean
+@RequestScoped
+public class CadastroIMC {
+
+    private String peso;
+    private String altura;
+    public String resultado;
+   
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+    
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String curso) {
+        this.altura = altura;
+    }  
+    
+    public String getResultado()
+    {
+        return resultado;
+    }
+    
+    public void setResultado(String resultado)
+    {
+        this.resultado = resultado;
+    }
+    
+    
+    
+    public CadastroIMC() {
+    }
+    
+    
+    
+    public String calculaDados() {
+        this.setResultado("O Resultado é: " + getResultado());
+    }
+}
